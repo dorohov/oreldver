335a6f51b876b4c530917e98bec47eb45a7061ef
caezar47 <iss47@ya.ru>
2016-03-14 08:45:10 +0300

    start

---
+++ src/js/document-ready.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	
+	[snp tpl="src/_/concat.document-ready.js" ]
+	
+	$(window).on('resize',function(event){
+		[snp tpl="src/_/concat.window-resize.js" ]
+	}).trigger('resize');
+	
+	$(window).on('scroll',function(){
+		[snp tpl="src/_/concat.window-scroll.js" ]
+	}).trigger('scroll');
+	
+	$('body').on('changeClass',function(){
+		[snp tpl="src/_/concat.body.changeClass.js" ]
+	});
+	
+	[snp tpl="src/_/concat.changeClass.js" ]
+	
+});
