b4fcaf3b552ca9ef9e5e44c0e0a177a7af5bf916
caezar47 <iss47@ya.ru>
2016-03-17 15:58:49 +0300

    edit css and pages

--- src/block/site/page/index/_b-service/.window-resize.js
+++ src/block/site/page/index/_b-service/.window-resize.js
@@ -1,4 +1,5 @@
 var t2 = $('.header-block').outerHeight(true),
 //t1 = $(window).height();
 t1 = $(window).outerHeight(true);
-$('.service-block').height(t1 - t2 );
+$('.service-block').height(t1 - t2);
+alert(1);
--- src/js/document-ready.js
+++ src/js/document-ready.js
@@ -1,6 +1,15 @@
+'use strict';
+
+window.onerror = function(error, url, lineNumber, column, errorObj) {
+	console.log('Error FECSS: ' + url + ':' + lineNumber + ':' + column + ': ' + JSON.stringify(error) + '\n' + JSON.stringify(errorObj));
+	return;
+}
+
 [snp tpl="src/_/concat.plugin.js" ]
 
 $(document).ready(function() {
+	
+	[snp tpl="src/_/concat.body.on.js" ]
 	
 	[snp tpl="src/_/concat.document-ready.js" ]
 	
@@ -17,5 +26,13 @@ $(document).ready(function() {
 	});
 	
 	[snp tpl="src/_/concat.changeClass.js" ]
+	
+	window.onbeforeunload = function(event) {
+		//alert(event.target.URL);
+		$('body').trigger('fecss.window.unload', [event]);
+		return;//return false;
+	}
+	
+	$(document.body).trigger('fecss.init');
 	
 });
